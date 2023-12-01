536935e720a16e14529c53743c89c5fc1024db59
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-01 09:42:31 -0800

    please accepted

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) - 1 == 0:
+from sys import argv
+if len(argv) - 1 == 0:
     print("0 arguments.")
-elif len(sys.argv) - 1 == 1:
-    print("1 argument:".format(sys.argv[1]))
-    print("{}: {}".format(1, sys.argv[1]))
+elif len(argv) - 1 == 1:
+    print("1 argument:".format(argv[1]))
+    print("{}: {}".format(1, argv[1]))
 else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+    print("{} arguments:".format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
 
