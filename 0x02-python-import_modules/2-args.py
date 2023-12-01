9d050230cf9fad079e5079fe6a14230ef3a215d8
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-01 10:43:15 -0800

    the task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     if (len(sys.argv) - 1) == 0:
         print("0 arguments.")
     elif (len(sys.argv) - 1) == 1:
-        print("1 argument: ")
+        print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
