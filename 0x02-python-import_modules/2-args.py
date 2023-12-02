1b8cd6478595a7e86ba5748bed7901d5c62c56f9
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-01 20:26:15 -0800

    args task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-    if (len(sys.argv) - 1) == 0:
+    from sys import argv
+    if (len(argv) - 1) == 0:
         print("0 arguments.")
-    elif (len(sys.argv) - 1) == 1:
+    elif (len(argv) - 1) == 1:
         print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
+        print("1: {}".format(argv[1]))
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
             
