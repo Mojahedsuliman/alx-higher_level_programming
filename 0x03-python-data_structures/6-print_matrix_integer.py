462b08a70c1a50ce310a8c0c85a82101e6676a1e
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-04 10:39:30 -0800

    the second correct

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{:d}".format(j), end="")
-            if j != i[-1]:
-                print(" ", end="")
+            print(" ".join("{:d}".format(j)), end="")
