99953c84c6161eac2686a2ae59afeed126977419
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-04 10:33:59 -0800

    my string task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for i in my_string:
-        if i != 'c' and i != 'C':
-            new_string += i
-            return new_string
+    new_string = my_string.translate({ord('c'): None})
+    new_string = new_string.translate({ord('C'): None})
+    return new_string
