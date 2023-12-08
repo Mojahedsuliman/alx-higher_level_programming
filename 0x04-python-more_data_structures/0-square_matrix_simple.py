d6d940793f99f6a39dea8cce994223eaf5da5f55
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-08 11:04:36 -0800

    pythonic

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            new_matrix[i][j] = matrix[i][j] ** 2
-            return new_matrix
+    new_matrix = [[y**2 for y in row] for row in matrix]
+    return new_matrix
