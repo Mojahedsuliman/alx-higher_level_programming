12501a38f422b42f474bf6cb2cf349eb534c6f43
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-08 11:49:04 -0800

    more data and structures

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    copy_list = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            copy_list.append(replace)
+        else:
+            copy_list.append(my_listi[i])
+            return copy_list
