71fb30028a276263d5474de886aed9fb6169b020
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-08 12:59:35 -0800

    the

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None
-    return None
-largest = 0
-large_key = None
-for key, value in a_dictonary.items():
-    if value > largest:
-        largest = value
-        large_key = key
-        return large_key
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    return max(a_dictionary, key=a_dictionary.get)
