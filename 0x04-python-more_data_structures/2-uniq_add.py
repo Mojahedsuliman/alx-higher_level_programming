fbc934f5034c020675aaaf36a78545fdb68cfeed
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-08 12:16:35 -0800

    the add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = set(my_list)
-    sum = 0
-    for i in new_list:
-        sum += i
-        return sum
+    result = 0
+    for i in set(my_list):
+        result += i
+        return result
