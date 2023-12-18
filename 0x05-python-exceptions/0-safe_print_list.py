fd05a02add5364b32d279a748b3fc063702c7f2f
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-18 11:25:21 -0800

    all task for python

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     y = 0
-    for i in range(x, len(my_list)):
+    for i in range(len(my_list) if x is None else x):
     try:
         print("{:d}".format(my_list[i]), end="")
         y += 1
