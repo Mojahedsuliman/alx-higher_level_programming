0300a68b0e5e4c0b5678680ceebfe2acaf929d30
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-19 05:08:25 -0800

    the rora

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """the square."""
+
+    def __init__(self, size=0):
+        """Initializes a square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """the square value now"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """the return value for square"""
+        return (self.__size) ** 2
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        for i in range(self.__size):
+            [print("#", end="")
+                    for j in range(self.__size)]
+            print("")
+            if self.__size == 0:
+                print("")
