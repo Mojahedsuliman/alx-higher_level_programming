792f1bed115d5d48d4fc81f21c13cb07241cbec7
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-18 23:54:12 -0800

    they

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
         print("0 arguments.")
     else:
         print("{:d} argument".format(len(argv) - 1), end="")
-        if len(srgv) > 2:
+        if len(argv) > 2:
             print("s", end="")
-        print(":")
-        for i in range(1, len(argv)):
+            print(":")
+            for i in range(1, len(argv)):
                 print("{:d}: {:s}".format(i, argv[i]))
