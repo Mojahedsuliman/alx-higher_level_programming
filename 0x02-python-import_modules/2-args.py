ef2f727bce0f210a7271c80f778124d62b4f237d
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-18 23:21:49 -0800

    the python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
+    import sys
     l = len(sys.argv) - 1
+
     if l == 0:
         print("{:d} arguments.".format(l))
     elif l == 1:
