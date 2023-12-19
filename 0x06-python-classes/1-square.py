8a5a1f84ce94fa4c73c7d8874dca24b26b7b1aac
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-19 03:35:41 -0800

    the sq

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """the square"""
+
+    def __init__(self, size):
+        """Initializes anew square
+
+        Args:
+        size (int): The size of the new square
+        """
+        self.__init__ = size
