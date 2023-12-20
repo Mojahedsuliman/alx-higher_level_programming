aa515ed25d2a64c5e85641f3e419d8f5c7ff43d2
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-20 11:57:01 -0800

    lkjhgfdds

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-def uppercase(s):
-    for c in s:
-        diff = ord('a') - ord('A')
-        if ord('a') >= ord(c) >= ord('a'):
-            print(chr(ord(c) - diff), end='')
-        else:
-            print(c, end='')
-    print()
+def uppercase(str):
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            c = chr(ord(c) - (ord('a') - ord('A')))
+        print("{:s}".format(c), end='')
+    print("")
