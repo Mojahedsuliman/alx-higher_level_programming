a198734beebe1e5f62d8cc38e5575a6d08bef605
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-20 11:12:42 -0800

    oiu

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    if len(my_list):
+    if not my_list:
         return None
-    return stored(my_list)[-1]
+    
+    max_num = my_list[0]
+    for num in my_list[1:]:
+        if num > max_num:
+            max_num = num
+    return max_num
