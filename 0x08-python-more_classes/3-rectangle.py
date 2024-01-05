bd6d9a98f29562a0c7742dbe279294499b1c9276
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-01-05 09:59:29 -0800

    the fourth

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -57,7 +57,9 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ("")
 
-        return ('\n').join(['#' * self.__width for _ in range(self.__height)])
-
-    def __repr__(self):
-        return f"<{type(self).__name__} object at {id(self):#x}>"
+        re = []
+        for i in range(self.__height):
+            [re.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                re.append("\n")
+        return ("".join(re))
