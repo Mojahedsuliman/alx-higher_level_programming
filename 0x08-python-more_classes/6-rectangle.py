8345be50927f8aafa3073d71284d7f37dfbd83ff
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-01-05 10:20:20 -0800

    the rectangle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -72,5 +72,5 @@ class Rectangle:
 
     def __del__(self):
         """Print deletion message"""
-        type(self).number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
