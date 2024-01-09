f868b6f3bbf93e82ab72669082a906a8cc2e67a9
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-01-09 11:35:59 -0800

    kokok

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Defines a a function that returns the dictionary description with simple data structure """
+"""Defines a a function that returns
+the dictionary description with simple data structure """
 
 
 def class_to_json(obj):
-    """Returns the dictionary description with simple data structure"""
-
+    """Returns the dictionary description
+    with simple data structure"""
     return (obj.__dict__)
