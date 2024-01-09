cb11fcff8dbb6234e2c469699c0d8c033651d15c
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-01-09 11:04:11 -0800

    hocker

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,6 +4,6 @@ import json
 
 
 def load_from_json_file(filename):
-        """JSON representation"""
-        with open(filename) as f:
-            return (json.load(f))
+    """JSON representation"""
+    with open(filename) as f:
+        return (json.load(f))
