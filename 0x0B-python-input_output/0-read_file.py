32062d2657e5286908abb528abc9363f0fe1e60d
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-01-09 10:19:51 -0800

    you are engineer

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a function that reads a text file using with"""
+ 
+
+def read_file(filename=""):
+    """prints the file content"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read(), end="")
