25e2de61f8673270f7b05b0425bf319b894f1da6
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-01-10 10:40:43 -0800

    locally

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
+
 def matrix_divided(matrix, div):
-
-    if not (isinstance(row, list) for row in matrix) or not (isinstance(elem, (int, float)) for row in matrix for elem in row):
+    if not isinstance(matrix, list) or not isinstance(row, list) for row in matrix:
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
-
-    row_lengths = [len(row) for row in matrix]
-    if len(set(row_lengths)) > 1:
+    if not len(row) == len(matrix[0]) for row in matrix:
         raise TypeError("Each row of the matrix must have the same size")
-
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = [[round(elem / div, 2) for elem in row] for row in matrix]
-    
-    return (new_matrix)
+    divided_matrix = []
+    for row in matrix:
+        divided_row = []
+        for elem in row:
+            if not isinstance(elem, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            divided_row.append(round(elem / div, 2))
+        divided_matrix.append(divided_row)
+    return (divided_matrix)
