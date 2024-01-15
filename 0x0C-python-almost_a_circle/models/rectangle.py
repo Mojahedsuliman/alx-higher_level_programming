7d0d3c60ef436e2b6ffd9987a33f8d3c9fee643c
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-01-15 10:54:18 -0800

    prprp

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,9 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Rectangle class, inherits from Base"""
+    """Rectangle class,
+    inherits from Base
+    """
 
 
     def __init__(self, width, height, x=0, y=0, id=None):
