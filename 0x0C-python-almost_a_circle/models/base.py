06e3c81ae86d836bd1d39ef1ccc76538aa8e5318
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-01-16 10:58:01 -0800

    bebo

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,8 +11,17 @@ class Base:
 
     def __init__(self, id=None):
         """Class constructor."""
+
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
