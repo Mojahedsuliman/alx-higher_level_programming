ed232a398ac58e3f8592c0d99acc88503c29a203
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-01-16 10:33:50 -0800

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,14 @@ class Square(Rectangle):
 
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        """Return a string representation of the Square instance"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+    @property
+    def size(self):
+        """Getter for size"""
+        return (self.width)
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        self.width = value
+        self.height = value
 
