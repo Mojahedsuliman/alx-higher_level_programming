e3efbab27af21794ae088e51c3e089174e9cd84c
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-02-14 12:31:57 -0800

    moly

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- This script that creates the MySQL server user user_0d_1.
-echo "CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';"
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';"
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
