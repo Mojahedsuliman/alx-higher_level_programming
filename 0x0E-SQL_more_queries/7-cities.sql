a763457b1a88dde7c1a368238544e9a182979794
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-02-15 13:30:33 -0800

    pipi

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,10 @@
 -- This part of script will create the database hbtn_0d_usa if not exists
-echo "CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;"
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- This part of script will create the table cities if not exists
-echo "CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id)
-);"
+);
