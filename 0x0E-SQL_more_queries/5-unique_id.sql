1e0bc5a7f8e4da095d18a1b36ab051af0c1c05ca
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-02-15 13:27:54 -0800

    pipi

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- This script create the table unique_id if not exists
-echo "CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));"
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
