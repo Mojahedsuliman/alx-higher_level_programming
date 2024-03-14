b1f80e1f55af2f9e49a4c6e97158a3d64b6d4208
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-03-13 17:55:21 -0700

    leko

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-function callMeMoby(x, theFunction) {
-	theFunction(++x);
+exports.callMeMoby = function (x, theFunction) {
+	for (let i = 0; i < x; i++) {
+		theFunction();
+	}
 };
