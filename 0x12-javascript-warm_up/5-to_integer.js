d7ab0b661cb8a9c7bd68e58fad4217a8b824e8ac
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-03-13 17:34:52 -0700

    task correc

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-if (isNaN(arg))
-{
+const num = parseInt(process.argv[2]);
+
+if (!isNaN(num)) {
+	console.log('My number:', num);
+} else {
 	console.log('Not a number');
-}
-else
-{
-	console.log('My number: ' + arg);
 }
