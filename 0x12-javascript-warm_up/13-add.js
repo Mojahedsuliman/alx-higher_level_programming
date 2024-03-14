da2db44058f1f84f65d97bf6ad4cb8744c233e04
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-03-13 17:47:56 -0700

    plop

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-function add (a, b)
-{
-    return (a + b);
+exports.add = function (a, b) {
+	return (a + b);
 }
-console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])))
