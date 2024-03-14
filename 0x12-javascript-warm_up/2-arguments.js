c7b723e9636a20241cfa13baedad608fa94451c0
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-03-13 17:06:04 -0700

    lpa

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (proccess.argv.length === 2) {
-	console.log("No argument");
+	console.log('No argument');
 } else if (proccess.argv.length === 3) {
-	console.log("Argument found");
+	console.log('Argument found');
 } else {
-	console.log("Arguments found");
-}
+	console.log('Arguments found');
+};
