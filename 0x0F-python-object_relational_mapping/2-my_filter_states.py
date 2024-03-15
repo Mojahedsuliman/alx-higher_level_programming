f4a61c93094c8e787a10221c8c3df8c46669742a
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-03-15 15:08:17 -0700

    plplplplo

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa
+Script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
 where name matches the argument.
 """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) < 5:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-        state_name = sys.argv[4]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    try:
-        database = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database,
-        )
-        cursor = database.cursor()
-        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-        cursor.execute(query, (state_name,))
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
-        cursor.close()
-        database.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
+    if len(sys.argv) == 4:
+        try:
+            db = MySQLdb.connect(host="localhost", 
+                port=3306,
+                user=username, 
+                passwd=password, 
+                db=database)
+            cursor = db.cursor()
+
+            query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+            cursor.execute(query, (state_name,))
+
+            states = cursor.fetchall()
+            for state in states:
+                print(state)
+
+            cursor.close()
+            db.close()
+
+        except MySQLdb.Error as e:
+            print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+            sys.exit(1)
