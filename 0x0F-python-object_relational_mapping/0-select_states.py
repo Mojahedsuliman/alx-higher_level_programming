f6f1ad1a52579c3842aaa6740d3416dfe77d5a88
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-03-15 11:08:43 -0700

    lopa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
-"""
-script that lists all states from the database hbtn_0e_0_usa
-"""
-
-import sys
 import MySQLdb
+import sys
 
-if __name__ = "__main__":
-    if len(sys.argv) == 4:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-        database = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user=username,
-                passwd=password,
-                db=database,
-                )
-        cursor = database.cursor()
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(
+                host="localhost", 
+                port=3306, 
+                user=username, 
+                passwd=password, 
+                db=database)
+        cursor = db.cursor()
         cursor.execute("SELECT * FROM states ORDER BY id ASC")
         states = cursor.fetchall()
         for state in states:
             print(state)
         cursor.close()
-        database.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
